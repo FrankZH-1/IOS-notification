fd0a35f1e42bf03c9e9454b1b899f4e37588c762
Zhao_Yifei <chinacpld@126.com>
2021-04-13 16:20:43 +0800

    ready-to-complete version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    document.querySelector(".volumeCover").onmousedown = function clickMove(event) {
+    document.querySelector(".volumeCover").addEventListener("mousedown",event =>{
         var barLeft = 0;
         var controlDot = document.querySelector(".controlDot");
         var volumeMoved = document.querySelector(".volumeBarMoved");
@@ -35,32 +35,32 @@ window.onload = function () {
             window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
         }
 
-        document.onmouseup = function () {
+        document.addEventListener("mouseup",()=> {
             document.onmousemove = null;
-        };
-    };
+        });
+    });
 
     document.querySelector(".deleteButton").onclick = function clickDelete() {
         var deleteButton = document.querySelector(".deleteButton");
         var textX = document.querySelector(".textX");
         var textDelete = document.querySelector(".textDelete");
+        var headerText = document.querySelector(".headerText");
 
         if (deleteButton.offsetWidth === 24) {
             textDelete.innerHTML = "清除";
             deleteButton.style.width = "40px";
-            textDelete.style.animation = "fadeIn 0.5s";
+            textDelete.style.animation = "fadeIn 0.3s";
             textDelete.style.opacity = "0.65";
-            textX.style.animation = "fadeOut 0.5s";
+            textX.style.animation = "fadeOut 0.3s";
             textX.style.opacity = "0";
         }
         else if (deleteButton.offsetWidth === 40) {
             textX.innerHTML = "X";
             deleteButton.style.width = "24px";
-            textDelete.style.animation = "fadeOut 0.5s";
-            textDelete.style.opacity = "0";
-            textX.style.animation = "fadeIn 0.5s";
-            textX.style.opacity = "0.65";
+            deleteButton.style.animation = headerText.style.animation = textDelete.style.animation = "fadeOut 0.5s";
+            deleteButton.style.opacity = headerText.style.opacity = textDelete.style.opacity = "0";
             clearNotiAll();
+
         }
 
         function clearNotiAll() {
@@ -74,15 +74,5 @@ window.onload = function () {
             }
         }
     }
-    
-    var climbStairs = function(n) {
-        var sum = 1;
-        function calculate(num){
-            for(let i = num;i > 0;i --){
-                sum *= i;
-            }
-        }
-        calculate(3);
-        console.log(calculate(3));
-    };
-}
+
+} 
